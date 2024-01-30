1ac293bd888e882057e4cba341870bb445775623
isoreus <iso.reus@gmail.com>
2024-01-30 17:44:22 +0300

    I'm preparing register API.

--- app/Helper/helper.php
+++ app/Helper/helper.php
@@ -2,6 +2,6 @@
 
 if(!function_exists('apiResponse')) {
     function apiResponse($message = null, $status =200, $data =null) {
-        return response() ->json(['message' => $message, 'data' =>$data],$status);
+        return response()->json(['message' => $message, 'data' =>$data],$status);
     }
 }
--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,14 +3,39 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function register(Request $request) {
-        $data=$request->all();
+        $requestData=$request->all();
 
-        apiResponse('Message',200,$data);
+        $this->validate($request, [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|unique:users',
+            'password' => 'required|string|min6'
+        ] ,
+        [
+            'name.required' => 'Name alanını doldurmak zorundasın.',
+            'name.string' => 'Text tipini string yapmalısın',
+            'name.max' => 'Max 255 karakterde olmalıdır.',
+            'email.required' => 'Email alanını doldurmak zorundasınız.',
+            'email.string' => 'Email alanı tip string olmalıdır.',
+            'email.unique' => 'Email zaten kayıtlıdır.',
+            'email.email' => 'Geçersiz email türü.',
+            'password.required' => 'Şifre alanı boş bırakılamaz',
+            'password.min'=> 'Şifre en az 6 karakterden olusmalıdır'
+        ]);
+
+        $data = User::create( [
+            'name' => $requestData['name'],
+            'email' => $requestData['email'],
+            'password' => Hash::make($requestData['password']) //şifremizi şifreliyorz
+        ]);
+
+        return apiResponse('Message',200,$data);
     }
 
     public function login() {
@@ -19,6 +44,6 @@ class UserController extends Controller
             'tss'
         ];
 
-        apiResponse('Test',200,$data);
+        return apiResponse('Test',200,$data);
     }
 }
